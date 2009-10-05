c9241586be9e02b4c005c0306b7f12ada62e567f
James Stuart <tastyhat@jamesstuart.org>
2009-10-05 15:12:19 -0400

    Initial commit to authlogic_wind.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'authlogic_wind'
+
+class Test::Unit::TestCase
+end
