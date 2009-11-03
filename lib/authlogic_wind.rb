0625bd684c889b8f02d9d0d2e43d42bc262aab19
James Stuart <tastyhat@jamesstuart.org>
2009-11-03 12:18:17 -0500

    fixed require file

--- lib/authlogic_wind.rb
+++ lib/authlogic_wind.rb
@@ -1,6 +1,6 @@
-require File.dirname(__FILE__) + "/authlogic_wind/acts_as_authentic"
-require File.dirname(__FILE__) + "/authlogic_wind/session"
-require File.dirname(__FILE__) + "/authlogic_wind/helper"
+require "authlogic_wind/acts_as_authentic"
+require "authlogic_wind/session"
+require "authlogic_wind/helper"
 
 ActiveRecord::Base.send(:include, AuthlogicWind::ActsAsAuthentic)
 Authlogic::Session::Base.send(:include, AuthlogicWind::Session)
